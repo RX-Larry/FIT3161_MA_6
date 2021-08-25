857ced15e83a094368bba2688e6f8b70b9f54c0c
peijiun chong <pcho0009@student.monash.edu>
2021-08-25 20:38:16 +0800

    Code debugging

--- ML code/gnn.py
+++ ML code/gnn.py
@@ -10,6 +10,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from nilearn import plotting
 
+import scipy.sparse as sp
+import tensorflow as tf
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.metrics import categorical_accuracy
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+from spektral.layers import GCNConv, GlobalSumPool
+from spektral.data import BatchLoader, Dataset, DisjointLoader, Graph
+from spektral.transforms.normalize_adj import NormalizeAdj
+from spektral.layers.pooling import TopKPool
+
+from sklearn.model_selection import train_test_split
+
+#%%
+
 # Load data from the pickle file
 datapath = 'data/connectivity_matrices.pkl'
 with open(datapath,'rb') as f:
@@ -31,22 +50,6 @@ for ij in np.ndindex(tangent_matrices.shape):
 n_samples = labels.shape[0]
 y=labels
 X=np.array([i for i in range(111)])
-#%%
-
-import scipy.sparse as sp
-import tensorflow as tf
-
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.losses import CategoricalCrossentropy
-from tensorflow.keras.metrics import categorical_accuracy
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-
-from spektral.layers import GCNConv, GlobalSumPool
-from spektral.data import BatchLoader, Dataset, DisjointLoader, Graph
-from spektral.transforms.normalize_adj import NormalizeAdj
-from spektral.layers.pooling import TopKPool
 
 #%%
 
@@ -80,73 +83,66 @@ class MyDataset(Dataset):
         for node in a:
             graphList.append(Graph(x=X, a=node, y=y))
         return graphList
-
-data = MyDataset(871, X, y, a=tangent_matrices, transforms=NormalizeAdj())
-
-
+    
 # Train/valid/test split
-idxs = np.random.permutation(len(data))
-split_va, split_te = int(0.8 * len(data)), int(0.9 * len(data))
-idx_tr, idx_va, idx_te = np.split(idxs, [split_va, split_te])
-data_tr = data[idx_tr]
-data_va = data[idx_va]
-data_te = data[idx_te]
+def train_valid_test_split(data, target, train_size, test_size):
+    valid_size = 1 - (train_size + test_size)
+    X1, X_test, y1, y_test = train_test_split(data, target, test_size = test_size, random_state= 33)
+    X_train, X_valid, y_train, y_valid = train_test_split(X1, y1, test_size = float(valid_size)/(valid_size+ train_size))
+    return X_train, X_valid, X_test, y_train, y_valid, y_test
+
+X_train, X_valid, X_test, y_train, y_valid, y_test = train_valid_test_split(tangent_matrices, y, train_size=0.8, test_size=0.1)
+
+data_tr = MyDataset(X_train.shape, X, y_train, a=X_train, transforms=NormalizeAdj())
+data_va = MyDataset(X_valid.shape, X, y_valid, a=X_valid, transforms=NormalizeAdj())
+data_te = MyDataset(X_test.shape, X, y_test, a=X_test, transforms=NormalizeAdj())
 
 # Data loaders
 loader_tr = DisjointLoader(data_tr, batch_size=batch_size, epochs=epochs)
 loader_va = DisjointLoader(data_va, batch_size=batch_size)
 loader_te = DisjointLoader(data_te, batch_size=batch_size)
-
-n_labels = data.n_samples
 #%%
 
 class MyFirstGNN(Model):
 
     def __init__(self, n_hidden, n_labels):
         super().__init__()
-        self.conv1 = GCNConv(n_hidden, activation='relu')
-        self.pool1 = TopKPool(ratio=0.5)
-        self.dropout1 = Dropout(0.5)
+        self.graph_conv = GCNConv(n_hidden)
+        self.pool = GlobalSumPool()
+        self.dropout = Dropout(0.5)
+        self.dense = Dense(n_labels, 'softmax')
         
-        self.conv2 = GCNConv(n_hidden, activation='relu')
-        self.pool2 = TopKPool(ratio=0.5)
-        self.dropout2 = Dropout(0.5)
+        # self.conv2 = GCNConv(n_hidden, activation='relu')
+        # self.pool2 = TopKPool(ratio=0.5)
+        # self.dropout2 = Dropout(0.5)
         
-        self.conv3 = GCNConv(n_hidden, activation='relu')
-        self.pool3 = GlobalSumPool(ratio=0.5)
-        self.dropout3 = Dropout(0.5)
+        # self.conv3 = GCNConv(n_hidden, activation='relu')
+        self.pool = GlobalSumPool(ratio=0.5)
+        # self.dropout3 = Dropout(0.5)
         
         self.dense = Dense(n_labels, activation='softmax')
 
     def call(self, inputs):
-        x, a, i = inputs
-        x = self.conv1([x, a])
-        x1, a1, i1 = self.pool1([x, a, i])
-        x1 = self.conv2([x1, a1])
-        x2, a2, i2 = self.pool2([x1, a1, i1])
-        x2 = self.conv3([x2, a2])
-        out = self.poo3([x2, i2])
+        # x, a, i = inputs
+        # x = self.conv1([x, a])
+        # x1, a1, i1 = self.pool1([x, a, i])
+        # x1 = self.conv2([x1, a1])
+        # x2, a2, i2 = self.pool2([x1, a1, i1])
+        # x2 = self.conv3([x2, a2])
+        # out = self.poo3([x2, i2])
+        # out = self.dense(out)
+        out = self.graph_conv(inputs)
+        out = self.dropout(out)
+        out = self.pool(out)
         out = self.dense(out)
         return out
     
-model = MyFirstGNN(32, n_labels)
-optimizer = Adam(lr=learning_rate)
-loss_fn = CategoricalCrossentropy()
+model = MyFirstGNN(32, 2)
+optimizer = Adam(learning_rate=learning_rate)
+loss_fn = BinaryCrossentropy(from_logits=True)
 
-# @tf.function(input_signature=loader_tr.tf_signature())  # Specify signature here
-# def train_step(inputs, target):
-#     # keep track of our gradient
-#     with tf.GradientTape() as tape: # automatic differentiation
-#         predictions = model(inputs, training=True)
-#         loss = loss_fn(target, predictions)
-#     # calculate the gradients using our tape and then update the model weights
-#     gradients = tape.gradient(loss, model.trainable_variables)
-#     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-# for batch in loader_tr:
-#     train_step(*batch)
-
-model.fit(loader_tr.load(), steps_per_epoch=loader_tr.steps_per_epoch, epochs=100)
+model.compile(optimizer=optimizer, loss=loss_fn, metrics=['accuracy'])
+model.fit(loader_tr.load(), steps_per_epoch=loader_tr.steps_per_epoch, epochs=epochs)
 
 model.summary()
 
